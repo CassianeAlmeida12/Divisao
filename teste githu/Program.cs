2ef879b5ffd5cf99bd70e4996f5ba19728a94159
Cassiane Almeida <cassiane.almeida@estudante.ifro.edu.br>
2023-06-05 16:59:58 -0400

    Quarto commit

--- teste githu/Program.cs
+++ teste githu/Program.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 
-Console.WriteLine("Informe o primeiro valor:");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Informe o segundo valor:");
-int b = int.Parse(Console.ReadLine());
-
-
 try
 {
-
+    Console.WriteLine("Informe o primeiro valor:");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe o segundo valor:");
+    int b = int.Parse(Console.ReadLine());
     int resultado = a / b;
     Console.WriteLine($"O resultado é:  { resultado}");
 }
-catch(Exception ex)
+catch(DivideByZeroException ex)//posso fazer mais de um catch ---- Esse catch é especifico
 {
-    Console.WriteLine("Algo de errado ocorreu");
+    Console.WriteLine($"Não é permitido divisão por zero! {ex.Message}");
+}
+catch (InvalidCastException ex)//posso fazer mais de um catch
+{
+    Console.WriteLine($"Conversão de string para letra não é permitida! {ex.Message}");
+}
+catch (Exception ex)//posso fazer mais de um catch -------- Esse é generico
+{
+    Console.WriteLine($"Ocorreu um erro! {ex.Message}");
 }

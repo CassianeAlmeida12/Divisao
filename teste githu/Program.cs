fe69df949876819caf633cbbfb7abbe7010c5c90
Cassiane Almeida <cassiane.almeida@estudante.ifro.edu.br>
2023-06-05 16:43:01 -0400

    commit teste

--- teste githu/Program.cs
+++ teste githu/Program.cs
@@ -1,9 +1,14 @@
-﻿try
+﻿using System;
+
+Console.WriteLine("Informe o primeiro valor:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o segundo valor:");
+int b = int.Parse(Console.ReadLine());
+
+
+try
 {
-    Console.WriteLine("Informe o primeiro valor:");
-    int a = int.Parse(Console.ReadLine());
-    Console.WriteLine("Informe o segundo valor:");
-    int b = int.Parse(Console.ReadLine());
+
     int resultado = a / b;
     Console.WriteLine($"O resultado é:  { resultado}");
 }
